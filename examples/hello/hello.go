3d32f1687de171c1b100eedad574155212d0ed8d
Yuheng Kuang <i@yuheng.io>
2018-11-07 09:57:49 -0800

    Demostrate interceptors.

--- examples/hello/hello.go
+++ examples/hello/hello.go
@@ -1,3 +1,4 @@
+// Package hello implements simplest hello service.
 // go:generate protoc --go_out=plugins=grpc:. hello_service.proto
 package hello
 
@@ -11,6 +12,9 @@ type Service struct{}
 
 // Hello implements gRPC call Hello()
 func (h *Service) Hello(ctx context.Context, req *HelloRequest) (*HelloResponse, error) {
+	if req.Name == "" {
+		return nil, fmt.Errorf("No name")
+	}
 	return &HelloResponse{
 		Message: fmt.Sprintf("Hello %s", req.Name),
 	}, nil
--- examples/main.go
+++ examples/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/kuangyh/swiffy"
 	"github.com/kuangyh/swiffy/examples/hello"
 )
 
+func logInterceptor(ctx context.Context, req interface{}) (interface{}, error) {
+	if reqProto, ok := req.(proto.Message); ok {
+		log.Printf("Request: %s", proto.MarshalTextString(reqProto))
+	}
+	return nil, nil
+}
+
 func main() {
+	opt := &swiffy.Options{
+		RequestInterceptors: []swiffy.RequestInterceptor{
+			logInterceptor,
+		},
+	}
+
 	var helloServ hello.HelloServiceServer = &hello.Service{}
-	http.Handle("/api/hello", swiffy.NewServiceHandler(helloServ, nil))
+	http.Handle("/api/hello", swiffy.NewServiceHandler(helloServ, opt))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
