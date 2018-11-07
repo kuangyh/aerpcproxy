7b94b83ce3a263bbbc759e9622fe692beade61d8
Yuheng Kuang <i@yuheng.io>
2018-11-07 09:47:02 -0800

    refactor, add interceptor support, remove public single method http handler

--- examples/main.go
+++ examples/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	var helloServ hello.HelloServiceServer = &hello.Service{}
-	swiffy.RegisterService(nil, "/api/hello", helloServ)
+	http.Handle("/api/hello", swiffy.NewServiceHandler(helloServ, nil))
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- swiffy.go
+++ swiffy.go
@@ -15,8 +15,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"reflect"
-	"strings"
-	"unicode"
 
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
@@ -53,77 +51,31 @@ func Error(status int, customMessage string) error {
 	}
 }
 
-type proxyHandler struct {
+// RequestInterceptor intercepts request before main handler, it can modify request as needed.
+// If non-nil res returned, all request interceptors after and main handler will be skipped but response interceptor will be run as normal.
+// If non-nil err returned, all processing will be skipped and error will be returnd to client.
+//
+// This happend to be compatible with gprc's UnaryHandler
+type RequestInterceptor func(ctx context.Context, req interface{}) (res interface{}, err error)
+
+// ResponseInterceptor intercepts response after main handler, modify response as needed.
+// If non-nil err returned, all response interceptors after will be skipped and error wil be returned to client.
+type ResponseInterceptor func(ctx context.Context, res interface{}) error
+
+// Options contains options like interceptors.
+type Options struct {
+	RequestInterceptors  []RequestInterceptor
+	ResponseInterceptors []ResponseInterceptor
+}
+
+type methodHandler struct {
 	// The backend function to call
 	backend reflect.Value
 	reqType reflect.Type
+	opt     *Options
 }
 
-func (h *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	format := "proto"
-	if v := r.Form["format"]; len(v) > 0 {
-		format = v[0]
-	}
-	var rb []byte
-	if v := r.Form["request"]; len(v) > 0 {
-		rb = ([]byte)(v[0])
-	} else {
-		var err error
-		rb, err = ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("read request from HTTP body failed, %v", err), 500)
-			return
-		}
-	}
-
-	req := reflect.New(h.reqType).Interface().(proto.Message)
-	if len(rb) > 0 {
-		var err error
-		switch format {
-		case "json":
-			err = jsonpb.Unmarshal(bytes.NewBuffer(rb), req)
-		case "proto":
-			err = proto.Unmarshal(rb, req)
-		case "text":
-			err = proto.UnmarshalText(string(rb), req)
-		default:
-			err = fmt.Errorf("unknown format %s", format)
-		}
-		if err != nil {
-			http.Error(w, fmt.Sprintf("failed parsing request, %v", err), 500)
-			return
-		}
-	}
-	ctx := r.Context()
-	ret := h.backend.Call([]reflect.Value{reflect.ValueOf(ctx), reflect.ValueOf(req)})
-	if err, ok := ret[1].Interface().(error); ok && err != nil {
-		statusCode := 500
-		if s, ok := err.(HTTPStatus); ok {
-			statusCode = s.HTTPStatus()
-		}
-		http.Error(w, err.Error(), statusCode)
-		return
-	}
-	res := ret[0].Interface().(proto.Message)
-	switch format {
-	case "json":
-		w.Header().Add("Content-Type", "text/json; charset=utf-8")
-		m := jsonpb.Marshaler{}
-		m.Marshal(w, res)
-	case "proto":
-		w.Header().Add("Content-Type", "application/x-protobuf")
-		rb, _ := proto.Marshal(res)
-		w.Write(rb)
-	case "text":
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		proto.MarshalText(w, res)
-	}
-}
-
-// NewHandler wraps a function to HTTP handler that can be directly serves requests.
-// Function should look like func(context.Context, *requestProto) (*responesProto, error)
-func NewHandler(fn interface{}) http.Handler {
+func newMethodHandler(fn interface{}, opt *Options) *methodHandler {
 	fnt := reflect.TypeOf(fn)
 	if fnt.Kind() != reflect.Func {
 		panic("fn is not a function")
@@ -141,43 +93,141 @@ func NewHandler(fn interface{}) http.Handler {
 		fnt.Out(1) != errType:
 		panic("fn should be like func(context.Context, *requestProto) (*responesProto, error)")
 	}
-	return &proxyHandler{
+	return &methodHandler{
 		backend: reflect.ValueOf(fn),
 		reqType: fnt.In(1).Elem(),
+		opt:     opt,
 	}
 }
 
-// RegisterService proxies all public methods of serv to mux.
-// URL for method FooBar is /{prefix}/foo_bar, when mux is nil, http.DefaultServeMux will be used,
+func (h *methodHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var err error
+	format := "json"
+	if v := r.Form["format"]; len(v) > 0 {
+		format = v[0]
+	}
+	var rb []byte
+	if v := r.Form["request"]; len(v) > 0 {
+		rb = ([]byte)(v[0])
+	} else {
+		rb, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("read request from HTTP body failed, %v", err), 400)
+			return
+		}
+	}
+
+	ctx := r.Context()
+	req := reflect.New(h.reqType).Interface()
+	if err := h.decode(req.(proto.Message), rb, format); err != nil {
+		http.Error(w, fmt.Sprintf("parse request failed, %v", err), 400)
+		return
+	}
+	var res interface{}
+	for _, ri := range h.opt.RequestInterceptors {
+		if res, err = ri(ctx, req); res != nil || err != nil {
+			break
+		}
+	}
+	if res == nil && err == nil {
+		ret := h.backend.Call([]reflect.Value{reflect.ValueOf(ctx), reflect.ValueOf(req)})
+		res = ret[0].Interface()
+		err, _ = ret[1].Interface().(error)
+	}
+	if err == nil {
+		for _, ri := range h.opt.ResponseInterceptors {
+			if err = ri(ctx, res); err != nil {
+				break
+			}
+		}
+	}
+	if err == nil {
+		err = h.encode(w, res, format)
+	}
+	if err != nil {
+		statusCode := 500
+		if s, ok := err.(HTTPStatus); ok {
+			statusCode = s.HTTPStatus()
+		}
+		http.Error(w, err.Error(), statusCode)
+	}
+}
+
+func (h *methodHandler) decode(dst proto.Message, src []byte, format string) error {
+	if len(src) == 0 {
+		return nil
+	}
+	switch format {
+	case "json":
+		return jsonpb.Unmarshal(bytes.NewBuffer(src), dst)
+	case "proto":
+		return proto.Unmarshal(src, dst)
+	case "text":
+		return proto.UnmarshalText(string(src), dst)
+	default:
+		return fmt.Errorf("unknown format %s", format)
+	}
+}
+
+func (h *methodHandler) encode(w http.ResponseWriter, res interface{}, format string) error {
+	var resProto proto.Message
+	var ok bool
+	if resProto, ok = res.(proto.Message); !ok || resProto == nil {
+		return fmt.Errorf("Cannot encode non proto or nil response")
+	}
+	switch format {
+	case "json":
+		w.Header().Add("Content-Type", "text/json; charset=utf-8")
+		m := jsonpb.Marshaler{}
+		m.Marshal(w, resProto)
+	case "proto":
+		w.Header().Add("Content-Type", "application/x-protobuf")
+		rb, _ := proto.Marshal(resProto)
+		w.Write(rb)
+	case "text":
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		proto.MarshalText(w, resProto)
+	}
+	return nil
+}
+
+type serviceHandler struct {
+	methods map[string]http.Handler
+}
+
+// NewServiceHandler creates an http.Handler that serves all public method of serv.
 //
 // Note that RegisterService exports all public method of serv, it would generally be safer to pass in an interface
 // instead of struct, to avoid unintentially exports methods that's not intended to serve externally.
-func RegisterService(mux *http.ServeMux, prefix string, serv interface{}) {
-	if mux == nil {
-		mux = http.DefaultServeMux
+func NewServiceHandler(serv interface{}, opt *Options) http.Handler {
+	if opt == nil {
+		opt = &Options{}
 	}
+	methods := map[string]http.Handler{}
 	servVal := reflect.ValueOf(serv)
 	servType := reflect.TypeOf(serv)
 	for i := 0; i < servType.NumMethod(); i++ {
-		m := servType.Method(i)
-		h := NewHandler(servVal.MethodByName(m.Name).Interface())
-		mux.Handle(prefix+"/"+camelCaseToUnderscore(m.Name), h)
+		mn := servType.Method(i).Name
+		methods[mn] = newMethodHandler(servVal.MethodByName(mn).Interface(), opt)
 	}
+	return &serviceHandler{methods: methods}
 }
 
-func camelCaseToUnderscore(s string) string {
-	var out strings.Builder
-	var prev rune
-	for i, c := range s {
-		if unicode.IsUpper(c) || unicode.IsDigit(c) {
-			if i > 0 && (!unicode.IsDigit(c) || !unicode.IsDigit(prev)) {
-				out.WriteRune('_')
-			}
-			out.WriteRune(unicode.ToLower(c))
-		} else {
-			out.WriteRune(c)
-		}
-		prev = c
+func (h *serviceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	var method string
+	if v := r.Form["method"]; len(v) > 0 {
+		method = v[0]
 	}
-	return out.String()
+	if method == "" {
+		http.Error(w, "No method parameter", 400)
+		return
+	}
+	var mh http.Handler
+	var ok bool
+	if mh, ok = h.methods[method]; !ok {
+		http.Error(w, "Method not found", 404)
+		return
+	}
+	mh.ServeHTTP(w, r)
 }
